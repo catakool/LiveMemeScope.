28946a19b89511143368f986b1515bcd11efbf87
catakool <catarina.castro4993@gmail.com>
2026-08-25 11:54:28 +0100

    Delete memescope_final_v11 directory

--- memescope_final_v11/src/app/api/radar/route.ts
+++
@@ -1,5 +0,0 @@
-import { NextResponse } from "next/server";
-import { getNewTokenRadarFeed } from "@/lib/newTokenRadar";
-export const dynamic = "force-dynamic";
-export const maxDuration = 30;
-export async function GET() { return NextResponse.json(await getNewTokenRadarFeed()); }
